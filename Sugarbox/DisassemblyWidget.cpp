7ef7e344d1a2a6692b4c1378a61a2cb3d76bf74e
Thomas Guillemin <thomas.guillemin@gmail.com>
2023-04-22 23:22:37 +0200

    add scroll bar (vertical / preliminary) to disassembly windows

--- Sugarbox/DisassemblyWidget.cpp
+++ Sugarbox/DisassemblyWidget.cpp
@@ -7,10 +7,12 @@ DisassemblyWidget::DisassemblyWidget(QWidget* parent )
    : QWidget(parent),
    disasm_window_(this),
    vertical_sb_(Qt::Vertical, this),
-   horizontal_sb_(Qt::Horizontal, this)
+   horizontal_sb_(Qt::Horizontal, this),
+   max_adress_(0xFFFF),
+   current_address_(0)
 
 {
-   
+   connect(&vertical_sb_, SIGNAL(valueChanged(int)), this, SLOT(OnValueChange(int)));
 }
 
 void DisassemblyWidget::paintEvent(QPaintEvent* /* event */)
@@ -20,7 +22,9 @@ void DisassemblyWidget::paintEvent(QPaintEvent* /* event */)
    int height = size().height() - 5;
 
    painter.fillRect(0, 0, width, height, QColor(220, 220, 220));
-   painter.drawText(10, 10, "TEST " );
+
+   QString text = QString("Adress : %1").arg(current_address_);
+   painter.drawText(10, 10, text);
 
 }
 
@@ -32,13 +36,36 @@ void DisassemblyWidget::resizeEvent(QResizeEvent* e)
       return;
    }
 
-   // Do all the stuff : 
-   // Set scrollbar
-   vertical_sb_.move(e->size().width() - vertical_sb_.size().width(),
-      e->size().height() - horizontal_sb_.size().height());
+   // Set scrollbar   
+   vertical_sb_.move(e->size().width() - vertical_sb_.sizeHint().width(), 0);
+   vertical_sb_.resize(vertical_sb_.sizeHint().width(), e->size().height() - horizontal_sb_.sizeHint().height());
 
-   horizontal_sb_.move(e->size().width() - vertical_sb_.size().width(),
-      e->size().height() - horizontal_sb_.size().height());
+   horizontal_sb_.move(0, e->size().height() - horizontal_sb_.sizeHint().height());
+   horizontal_sb_.resize(e->size().width() - vertical_sb_.sizeHint().width(), horizontal_sb_.sizeHint().height());
 
+   ComputeScrollArea();
+
+}
+
+void DisassemblyWidget::OnValueChange(int valueScrollBar)
+{
+   current_address_ = static_cast<unsigned int>(valueScrollBar);
+   // Update;
+   repaint();
+}
+
+void DisassemblyWidget::SetDisassemblyInfo(unsigned int max_adress)
+{
+   max_adress_ = max_adress;
+}
+
+void DisassemblyWidget::ComputeScrollArea()
+{
+   // Vertical:
+   // Compute number of lines : 0 -> max_adress_
+   vertical_sb_.setMaximum(max_adress_);
+   vertical_sb_.setMinimum(0);
+   vertical_sb_.setSingleStep(1);
+   vertical_sb_.setPageStep(15); // TODO
 
 }
--- Sugarbox/DisassemblyWidget.h
+++ Sugarbox/DisassemblyWidget.h
@@ -10,12 +10,24 @@ class DisassemblyWidget : public QWidget
 public:
    explicit DisassemblyWidget(QWidget* parent = nullptr);
 
+   void SetDisassemblyInfo(unsigned int max_adress);
+
 protected:
     void paintEvent(QPaintEvent* event) override;
     void resizeEvent(QResizeEvent* e) override;
+
+    void ComputeScrollArea();
+
+public slots:
+    void OnValueChange(int valueScrollBar);
+
 
 private:
    QWidget disasm_window_;
    QScrollBar vertical_sb_;
    QScrollBar horizontal_sb_;
+
+   unsigned int max_adress_;
+   unsigned int current_address_;
+   
 };
