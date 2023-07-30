19d98316f7da80e60ab550ad3fd4fe30c9bfa9a0
Thomas Guillemin <thomas.guillemin@gmail.com>
2023-07-30 19:01:47 +0200

    Add more script commands

--- Sugarbox/Script.cpp
+++ Sugarbox/Script.cpp
@@ -29,6 +29,46 @@ Emulation* ScriptContext::GetEmulation()
    return emulation_;
 }
 
+void ScriptContext::SetDriveDir(std::filesystem::path path)
+{
+   drive_dir_ = path;
+}
+
+std::filesystem::path ScriptContext::GetDriveDir()
+{
+   return drive_dir_;
+}
+
+void ScriptContext::SetTapeDir(std::filesystem::path path)
+{
+   tape_dir_ = path;
+}
+
+std::filesystem::path ScriptContext::GetTapeDir()
+{
+   return tape_dir_;
+}
+
+void ScriptContext::SetSnapshotDir(std::filesystem::path path)
+{
+   snapshot_dir_= path;
+}
+
+std::filesystem::path ScriptContext::GetSnapshotDir()
+{
+   return snapshot_dir_;
+}
+
+void ScriptContext::SetScreenshotDir(std::filesystem::path path)
+{
+   screenshot_dir_ = path;
+}
+
+std::filesystem::path ScriptContext::GetScreenshotDir()
+{
+   return screenshot_dir_;
+}
+
 
 Script::Script(IScriptCommand* command, std::vector<std::string> parameter) : command_(command), parameter_(parameter)
 {
@@ -57,29 +97,28 @@ void ScriptCommandFactory::InitFactory(ScriptContext* context)
 
    AddCommand(new CommandCslVersion(), { "csl_version" });
    AddCommand(new CommandReset(), { "reset" });
-   /*
-   AddCommand(new RemoteCommandSetBreakpoint(), { "crtc_select" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "disk_insert" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "disk_dir" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "tape_insert" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "tape_dir" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "tape_play" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "tape_stop" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "tape_rewind" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "snapshot_load" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "snapshot_dir" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "key_delay" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "key_output" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "key_from_file" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "wait" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "wait_driveonoff" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "wait_vsyncoffon" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "screenshot_name" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "screenshot_dir" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "screenshot" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "snapshot_name" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "snapshot" });
-   AddCommand(new RemoteCommandSetBreakpoint(), { "csl_load" });*/
+   AddCommand(new CommandCrtcSelect(), { "crtc_select" });
+   AddCommand(new CommandDiskInsert(), { "disk_insert" });
+   AddCommand(new CommandDiskDir(), { "disk_dir" });
+   AddCommand(new CommandTapeInsert(), { "tape_insert" });
+   AddCommand(new CommandTapeDir(), { "tape_dir" });
+   AddCommand(new CommandTapePlay(), { "tape_play" });
+   AddCommand(new CommandTapeStop(), { "tape_stop" });
+   AddCommand(new CommandTapeRewind(), { "tape_rewind" });
+   AddCommand(new CommandSnapshotLoad(), { "snapshot_load" });
+   AddCommand(new CommandSnapshotDir(), { "snapshot_dir" });
+   AddCommand(new CommandKeyDelay(), { "key_delay" });
+   AddCommand(new CommandKeyOutput(), { "key_output" });
+   AddCommand(new CommandKeyFromFile(), { "key_from_file" });
+   AddCommand(new CommandWait(), { "wait" });
+   AddCommand(new CommandWaitDriveOnOff(), { "wait_driveonoff" });
+   AddCommand(new CommandWaitVSyncOffOn(), { "wait_vsyncoffon" });
+   AddCommand(new CommandScreenshotName(), { "screenshot_name" });
+   AddCommand(new CommandScreenshotDir(), { "screenshot_dir" });
+   AddCommand(new CommandScreenshot(), { "screenshot" });
+   AddCommand(new CommandSnapshotName(), { "snapshot_name" });
+   AddCommand(new CommandSnapshot(), { "snapshot" });
+   AddCommand(new CommandCslLoad(), { "csl_load" });
 
 }
 
@@ -147,6 +186,161 @@ bool CommandReset::Execute(std::vector<std::string>& param)
       // Reset all RAM
       // TODO
    }
+
+   return true;
+}
+
+////////////////////////////////////////////////////////
+/// crtc_select
+///
+bool CommandCrtcSelect::Execute(std::vector<std::string>& param)
+{
+   
+   if (param.size() > 2)
+   {
+      CRTC::TypeCRTC type_crtc = CRTC::HD6845S;
+
+      if (param[1] == "0") type_crtc = CRTC::HD6845S;
+      else if (param[1] == "1") type_crtc = CRTC::UM6845R;
+      else if (param[1] == "1A") type_crtc = CRTC::UM6845R;
+      else if (param[1] == "1B") type_crtc = CRTC::UM6845R;
+      else if (param[1] == "2") type_crtc = CRTC::MC6845;
+      else if (param[1] == "3") type_crtc = CRTC::AMS40489;
+      else if (param[1] == "4") type_crtc = CRTC::AMS40226;
+      else if (param[1] == "PUSSY") type_crtc = CRTC::UM6845R; // ?
+
+      context_->GetEmulation()->GetEngine()->GetCRTC()->DefinirTypeCRTC(type_crtc);
+   }
+   
+   return true;
+}
+
+////////////////////////////////////////////////////////
+/// disk_insert <drive> <’file with extension’ >
+///
+bool CommandDiskInsert::Execute(std::vector<std::string>& param)
+{
+   unsigned int drive = 0;
+   unsigned int param_name = 1;
+   if (param.size() == 3)
+   {
+      drive = (param[1] == "0") ? 0 : 1;
+      param_name = 2;
+   }
+
+   std::filesystem::path p = context_->GetDriveDir();
+   p += param[param_name];
+   context_->GetEmulation()->GetEngine()->LoadDisk(p.string().c_str(), drive);
+   
+
+   return true;
+}
+
+////////////////////////////////////////////////////////
+/// disk_dir <’disk directory’> 
+///
+bool CommandDiskDir::Execute(std::vector<std::string>& param)
+{
+   unsigned int drive = 0;
+   unsigned int param_name = 1;
+   if (param.size() < 2)
+   {
+      return true;
+   }
+
+   context_->SetDriveDir(param[param_name]);
+
+   return true;
+}
+
+////////////////////////////////////////////////////////
+/// tape_insert <’file with extension’>
+///
+bool CommandTapeInsert::Execute(std::vector<std::string>& param)
+{
+   unsigned int param_name = 1;
+   if (param.size() < 2)
+   {
+      return true;
+   }
+
+   std::filesystem::path p = context_->GetTapeDir();
+   p += param[1];
+   context_->GetEmulation()->GetEngine()->LoadTape(p.string().c_str());
+
+   return true;
+}
+
+
+////////////////////////////////////////////////////////
+/// tape_dir <’tape directory’> 
+///
+bool CommandTapeDir::Execute(std::vector<std::string>& param)
+{
+   if (param.size() < 2)
+   {
+      return true;
+   }
+
+   context_->SetTapeDir(param[1]);
+
+   return true;
+}
+
+////////////////////////////////////////////////////////
+/// tape_play 
+///
+bool CommandTapePlay::Execute(std::vector<std::string>& param)
+{
+   context_->GetEmulation()->TapePlay();
+   return true;
+}
+
+////////////////////////////////////////////////////////
+/// tape_stop 
+///
+bool CommandTapeStop::Execute(std::vector<std::string>& param)
+{
+   context_->GetEmulation()->TapeStop();
+   return true;
+}
+
+////////////////////////////////////////////////////////
+/// tape_rewind 
+///
+bool CommandTapeRewind::Execute(std::vector<std::string>& param)
+{
+   context_->GetEmulation()->TapeRewind();
+   return true;
+}
+
+////////////////////////////////////////////////////////
+/// snapshot_load 
+///
+bool CommandSnapshotLoad::Execute(std::vector<std::string>& param)
+{
+   if (param.size() < 2)
+   {
+      return true;
+   }
+   std::filesystem::path p = context_->GetSnapshotDir();
+   p += param[1];
+   context_->GetEmulation()->LoadSnapshot(p.string().c_str());
+   return true;
+}
+
+
+////////////////////////////////////////////////////////
+/// snapshot_dir 
+///
+bool CommandSnapshotDir::Execute(std::vector<std::string>& param)
+{
+   if (param.size() < 2)
+   {
+      return true;
+   }
+
+   context_->SetSnapshotDir(param[1]);
 
    return true;
 }
--- Sugarbox/Script.h
+++ Sugarbox/Script.h
@@ -13,12 +13,25 @@ public:
    void Init(Emulation* emulation);
 
    void SetVersion(const std::string& version);
+   void SetDriveDir(std::filesystem::path path);
+   void SetTapeDir(std::filesystem::path path);
+   void SetSnapshotDir(std::filesystem::path path);
+   void SetScreenshotDir(std::filesystem::path path);
 
    Emulation* GetEmulation();
+   std::filesystem::path GetDriveDir();
+   std::filesystem::path GetTapeDir();
+   std::filesystem::path GetSnapshotDir();
+   std::filesystem::path GetScreenshotDir();
+   
 
 protected:
    Emulation* emulation_;
    std::string version_;
+   std::filesystem::path drive_dir_;
+   std::filesystem::path tape_dir_;
+   std::filesystem::path snapshot_dir_;
+   std::filesystem::path screenshot_dir_;
 };
 
 class IScriptCommand
@@ -80,3 +93,134 @@ public:
    virtual bool Execute(std::vector<std::string>&);
 };
 
+class CommandCrtcSelect : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandDiskInsert : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandDiskDir : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandTapeInsert : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandTapeDir : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandTapePlay : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandTapeStop : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandTapeRewind : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandSnapshotLoad : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandSnapshotDir : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandKeyDelay: public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandKeyOutput : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandKeyFromFile : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandWait : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandWaitDriveOnOff : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandWaitVSyncOffOn: public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandScreenshotName : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandScreenshotDir : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandScreenshot : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandSnapshotName : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandSnapshot : public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
+
+class CommandCslLoad: public IScriptCommand
+{
+public:
+   virtual bool Execute(std::vector<std::string>&);
+};
