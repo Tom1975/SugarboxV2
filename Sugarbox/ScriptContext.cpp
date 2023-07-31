3aa4c720efab3e98a4735e290006c0cc838e8f6f
Thomas GUILLEMIN <thomas.guillemin@conduent.com>
2023-07-31 15:56:23 +0200

    Refactor : class splitting

---
+++ Sugarbox/ScriptContext.cpp
@@ -0,0 +1,94 @@
+
+#include "ScriptContext.h"
+#include "Emulation.h"
+
+ScriptContext::ScriptContext() : emulation_(nullptr), delay_(0), delay_cr_(0)
+{
+   
+}
+
+ScriptContext::~ScriptContext()
+{
+   
+}
+
+void ScriptContext::Init(Emulation* emulation)
+{
+   emulation_ = emulation;
+}
+
+void ScriptContext::SetVersion(const std::string& version)
+{
+   version_ = version;
+}
+
+Emulation* ScriptContext::GetEmulation()
+{
+   return emulation_;
+}
+
+void ScriptContext::SetDriveDir(std::filesystem::path path)
+{
+   drive_dir_ = path;
+}
+
+std::filesystem::path ScriptContext::GetDriveDir()
+{
+   return drive_dir_;
+}
+
+void ScriptContext::SetTapeDir(std::filesystem::path path)
+{
+   tape_dir_ = path;
+}
+
+std::filesystem::path ScriptContext::GetTapeDir()
+{
+   return tape_dir_;
+}
+
+void ScriptContext::SetSnapshotDir(std::filesystem::path path)
+{
+   snapshot_dir_= path;
+}
+
+std::filesystem::path ScriptContext::GetSnapshotDir()
+{
+   return snapshot_dir_;
+}
+
+void ScriptContext::SetSnapshotName(std::string path)
+{
+   snapshot_name_ = path;
+}
+
+std::filesystem::path ScriptContext::GetSnapshotName()
+{
+   return snapshot_dir_;
+}
+
+void ScriptContext::SetScreenshotDir(std::filesystem::path path)
+{
+   screenshot_dir_ = path;
+}
+
+std::filesystem::path ScriptContext::GetScreenshotDir()
+{
+   return screenshot_dir_;
+}
+
+void ScriptContext::SetScreenshotName(std::string path)
+{
+   screenshot_name_ = path;
+}
+
+std::filesystem::path ScriptContext::GetScreenshotName()
+{
+   return screenshot_name_;
+}
+
+void ScriptContext::SetKeyDelay(unsigned int delay, unsigned int delay_cr)
+{
+   delay_ = delay;
+   delay_cr_ = delay_cr_;
+}
---
+++ Sugarbox/ScriptContext.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <filesystem>
+
+class Emulation;
+
+class ScriptContext
+{
+public:
+   ScriptContext();
+   virtual ~ScriptContext();
+
+   void Init(Emulation* emulation);
+
+   void SetVersion(const std::string& version);
+   void SetDriveDir(std::filesystem::path path);
+   void SetTapeDir(std::filesystem::path path);
+   void SetSnapshotDir(std::filesystem::path path);
+   void SetSnapshotName(std::string path);
+   void SetScreenshotDir(std::filesystem::path path);
+   void SetScreenshotName(std::string path);
+
+   void SetKeyDelay(unsigned int delay, unsigned int delay_cr);
+
+   Emulation* GetEmulation();
+   std::filesystem::path GetDriveDir();
+   std::filesystem::path GetTapeDir();
+   std::filesystem::path GetSnapshotDir();
+   std::filesystem::path GetSnapshotName();
+   std::filesystem::path GetScreenshotDir();
+   std::filesystem::path GetScreenshotName();
+
+   
+
+protected:
+   Emulation* emulation_;
+   std::string version_;
+   std::filesystem::path drive_dir_;
+   std::filesystem::path tape_dir_;
+   std::filesystem::path snapshot_dir_;
+   std::string snapshot_name_;
+   std::filesystem::path screenshot_dir_;
+   std::string screenshot_name_;
+
+   unsigned int delay_;
+   unsigned int delay_cr_;
+};
